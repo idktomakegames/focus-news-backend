6059d260b80efd2ad4651ddd7d78073daf77f013
Ionut <ghitadumveanu@gmail.com>
2025-05-01 10:54:48 +0300

    commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+app.use(cors({origin: 'https://focus-news.netlify.app', credentials: true}));
 const limiter = rateLimit({
     windowMs: 10 * 60000,
     max: 300,
